d7085ed0afd9e1ae2406e005848a29c423ada60e
ishaniekanayaka <ishaniekanayaka27@gmail.com>
2024-12-15 10:14:48 +0530

    changed the orderHistory_model.js file

--- models/OrderHistory_Model.js
+++ models/OrderHistory_Model.js
@@ -1,4 +1,4 @@
-export default class orderDetailsModel{
+export default class OrderHistory_Model {
     constructor(order_id, date, customerId, itemName, itemPrice, order_qty, sub_total, discount_range, discount, final_total) {
         this._order_id = order_id;
         this._date = date;
@@ -12,83 +12,85 @@ export default class orderDetailsModel{
         this._final_total = final_total;
     }
 
+    // Getters
     get order_id() {
         return this._order_id;
-    }
-
-    set order_id(value) {
-        this._order_id = value;
     }
 
     get date() {
         return this._date;
     }
 
-    set date(value) {
-        this._date = value;
-    }
-
     get customerId() {
         return this._customerId;
-    }
-
-    set customerId(value) {
-        this._customerId = value;
     }
 
     get itemName() {
         return this._itemName;
     }
 
-    set itemName(value) {
-        this._itemName = value;
-    }
-
     get itemPrice() {
         return this._itemPrice;
-    }
-
-    set itemPrice(value) {
-        this._itemPrice = value;
     }
 
     get order_qty() {
         return this._order_qty;
     }
 
-    set order_qty(value) {
-        this._order_qty = value;
-    }
-
     get sub_total() {
         return this._sub_total;
-    }
-
-    set sub_total(value) {
-        this._sub_total = value;
     }
 
     get discount_range() {
         return this._discount_range;
     }
 
-    set discount_range(value) {
-        this._discount_range = value;
-    }
-
     get discount() {
         return this._discount;
-    }
-
-    set discount(value) {
-        this._discount = value;
     }
 
     get final_total() {
         return this._final_total;
     }
 
-    set final_total(value) {
-        this._final_total = value;
+    // Setters
+    set order_id(order_id) {
+        this._order_id = order_id;
+    }
+
+    set date(date) {
+        this._date = date;
+    }
+
+    set customerId(customerId) {
+        this._customerId = customerId;
+    }
+
+    set itemName(itemName) {
+        this._itemName = itemName;
+    }
+
+    set itemPrice(itemPrice) {
+        this._itemPrice = itemPrice;
+    }
+
+    set order_qty(order_qty) {
+        this._order_qty = order_qty;
+    }
+
+    set sub_total(sub_total) {
+        this._sub_total = sub_total;
+    }
+
+    set discount_range(discount_range) {
+        this._discount_range = discount_range;
+    }
+
+    set discount(discount) {
+        this._discount = discount;
+    }
+
+    set final_total(final_total) {
+        this._final_total = final_total;
     }
 }
